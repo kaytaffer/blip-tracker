27d35b7f150102eeb90e8c8959f7f32445d7bec9
kaytaffer <kevinheltenkelt@gmail.com>
2024-10-17 11:15:30 +0200

    Initial frame settings

--- src/view/TrackerFrame.java
+++ src/view/TrackerFrame.java
@@ -1,5 +1,6 @@
 package view;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 /**
@@ -7,7 +8,25 @@ import javax.swing.JFrame;
  *
  */
 public class TrackerFrame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final String APPLICATION_NAME = "Bliptracker";
+	private final int X_FRAME_SIZE = 500;
+	private final int Y_FRAME_SIZE = 500;
+	private final boolean APP_IS_RESIZABLE = false; 
+	private final String ICON_PATH = "map.gif";
+	
 	public TrackerFrame() {
+		this.setTitle(APPLICATION_NAME);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(APP_IS_RESIZABLE);
+		this.setSize(this.X_FRAME_SIZE, this.Y_FRAME_SIZE);
+		this.setVisible(true);
 		
+		ImageIcon icon = new ImageIcon(ICON_PATH);
+		this.setIconImage(icon.getImage());
 	}
 }
