37f6b256ca6e0222951ce29e2312447c5ddc0b70
kaytaffer <kevinheltenkelt@gmail.com>
2024-10-18 11:38:11 +0200

    Renders visible blips

--- src/view/MapPanel.java
+++ src/view/MapPanel.java
@@ -1,27 +1,77 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import model.Blip;
+
+/**
+ * View component responsible for rendering the background map as well as the moving objects on a 2D surface.
+ */
 public class MapPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private final int BLIP_DIAMETER = 10;
+	private Map<Long, Blip> blipsToRender; 
+	private int[] visibleBlipTypes;
 	
-	public MapPanel(Dimension mapSize) {
-        
+	/**
+	 * Creates an instance of a {@code MapPanel}. 
+	 * @param mapSize The requested dimensions of the resulting {@code JPanel}.
+	 */
+	public MapPanel(Dimension mapSize) {    
         this.setPreferredSize(mapSize);
-        this.setLayout(null); // Disable layout manager for absolute positioning
-        // this.setBackground(new Color(0xA231FF)); // Purple background to test map scaling
-
-        JLabel mapLabel = new JLabel();
-        ImageIcon map = new ImageIcon("map.gif");
-        mapLabel.setIcon(map);
-        mapLabel.setBounds(0, 0, mapSize.width, mapSize.height); // Position at (0,0) with and square of mapSize
-      
-        this.add(mapLabel);
+        this.blipsToRender = new HashMap<>();
+        this.visibleBlipTypes = new int[] {1, 2, 3}; //Defaults to all types visible.
     }
+
+	/**
+	 * See {@code Component} documentation. This method is called implicitly and should not be called 
+	 * directly once this {@code MapPanel} has been instantiated.
+	 */
+	public void paint(Graphics g) {
+		Image mapImage = new ImageIcon("map.gif").getImage();
+		g.drawImage(mapImage, 0, 0, null);
+		
+		for(Blip blip : blipsToRender.values()) {
+			int blipType = blip.getType();
+			
+			for(int visibleType : visibleBlipTypes) {
+				if(blip.getType() == visibleType) {
+					if(blipType == 1)
+						g.setColor(Color.BLUE);
+					else if(blipType == 2)
+						g.setColor(Color.RED);
+					else if(blipType == 3)
+						g.setColor(Color.ORANGE);
+					g.fillOval(blip.getXCoordinate(), blip.getYCoordinate(), BLIP_DIAMETER, BLIP_DIAMETER);
+					break;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Re-renders this {@code MapPanel} with the supplied data.
+	 * @param blipsToRender The current state of the blips to render.
+	 */
+	public void renderBlips(Map<Long, Blip> blipsToRender) {
+		this.blipsToRender = blipsToRender;
+		repaint();
+	}
 	
+	/**
+	 * Sets which types of blips should be visible and rendered by this {@code MapPanel}.
+	 * @param visibleBlipTypes Which types should be visible.
+	 */
+	public void setVisibleBlipTypes(int[] visibleBlipTypes) {
+		this.visibleBlipTypes = visibleBlipTypes;
+	}
 }
