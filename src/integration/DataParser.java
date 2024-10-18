73f83b9a5ad07ac612cba27f38f21068f4b1c081
kaytaffer <kevinheltenkelt@gmail.com>
2024-10-18 14:46:59 +0200

    Sets up socket and listens for data from independent server.

--- src/integration/DataParser.java
+++ src/integration/DataParser.java
@@ -1,22 +1,46 @@
 package integration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.Socket;
+
 /**
- * TODO JavaDoc
- *
+ * Sets up a Socket that listens to localhost:5463 and parses what it receives 
+ * TODO Observers notification, decide on format to parse Strings to
  */
-public class DataParser {
-	/* TODO read and parse data inputed in format ID=<LONG>;X=<INT>;Y=<INT>;TYPE=<INT> 
-	 * 
-	 * 
-	 * ...
-	BufferedReader in = null;
-	Socket s = new Socket(”localhost”, 5463);
-	in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-	...
-	String inData;
-	while ((inData = in.readLine()) != null) {
-	 //Read and parse data
-	  System.out.println(inData);
+public class DataParser implements Runnable {
+	
+	private Socket socket; 
+	
+	public DataParser() {		
 	}
-	 */
+
+	@Override
+	public void run() {
+
+		try {
+			this.socket = new Socket("localhost", 5463);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			String inData;
+			while ((inData = in.readLine()) != null) {
+				parseData(inData);	
+			}
+		} catch (IOException iOException) {
+			if(iOException.getClass() == ConnectException.class)
+				System.out.println("Failed to connect to object data server. Please make sure server is running correctly, then restart application.");
+			iOException.printStackTrace();
+		}
+		
+	}
+
+	private void parseData(String data) {
+		
+		System.out.println(data);
+		/* TODO read and parse data inputed in format ID=<LONG>;X=<INT>;Y=<INT>;TYPE=<INT> 
+		 */
+		
+	}
 }
