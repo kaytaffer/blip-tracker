0f8bb7513f38cb2a2fa5ea579686066b202a86dc
kaytaffer <kevinheltenkelt@gmail.com>
2024-10-18 19:45:42 +0200

    All types visible by default, set visibility by user action.

--- src/bliptracker/view/OptionPanel.java
+++ src/bliptracker/view/OptionPanel.java
@@ -9,23 +9,36 @@ import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+/**
+ * A custom {@code JPanel} subclass for rendering and managing radio buttons the application user 
+ * can use to select which types of blip objects are visible.
+ */
 public class OptionPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private final MapPanel MAP_PANEL;
+	private MapPanel mapPanel;
+	private JRadioButton displayCategoryOneButton;
+	private JRadioButton displayCategoryTwoButton;
+	private JRadioButton displayAllButton;
 	
+	/**
+	 * Creates an instance of an {@code OptionPanel}. 
+	 * @param size The requested dimensions of the resulting {@code JPanel}.
+	 * @param mapPanel A {@code MapPanel} to inform when user selects their chosen options.
+	 */
 	public OptionPanel(Dimension size, MapPanel mapPanel) {
-		this.MAP_PANEL = mapPanel;
+		this.mapPanel = mapPanel;
 		this.setLayout(new GridLayout(3, 1, 0, 5)); //3 rows, one for each optional button. 5 pixel spacing.
 		this.setBackground(new Color(0xDDDDDD));
 		this.setPreferredSize(size);
 		
-		JRadioButton displayCategoryOneButton = new JRadioButton("Show Category 1");
-		JRadioButton displayCategoryTwoButton = new JRadioButton("Show Category 2");
-		JRadioButton displayAllButton = new JRadioButton("Show all");
+		this.displayCategoryOneButton = new JRadioButton("Show Category 1");
+		this.displayCategoryTwoButton = new JRadioButton("Show Category 2");
+		this.displayAllButton = new JRadioButton("Show all");
 		displayCategoryOneButton.addActionListener(event -> listenToButtons(event));
 		displayCategoryTwoButton.addActionListener(event -> listenToButtons(event));
 		displayAllButton.addActionListener(event -> listenToButtons(event));
+		displayAllButton.setSelected(true); //default to all types visible
 		
 		ButtonGroup buttonGroup = new ButtonGroup();
 		
@@ -38,9 +51,15 @@ public class OptionPanel extends JPanel {
 		this.add(displayAllButton);
 	}
 	
+	// Method to perform when action listener is triggered: Set visibility of blip types in mapPanel.
 	private void listenToButtons(ActionEvent buttonChoice) {
-		//TODO implement
-		System.out.println(buttonChoice.getSource().toString());
+		Object selectedButton = buttonChoice.getSource();
+		if(selectedButton == displayCategoryOneButton)
+			mapPanel.setVisibleBlipTypes(new int[] {1, 2});
+		else if(selectedButton == displayCategoryTwoButton) 
+			mapPanel.setVisibleBlipTypes(new int[] {3});
+		else if(selectedButton == displayAllButton)
+			mapPanel.setVisibleBlipTypes(new int[] {1, 2, 3});
 	}
 
 }
